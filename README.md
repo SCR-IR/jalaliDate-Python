416f9a4b9151505eb3d2775f07122b696301733b
SCR-IR <gholampanahi@email.scr.ir>
2020-08-30 00:17:06 +0430

    v2.80.x

---
+++ README.md
@@ -0,0 +1,29 @@
+# jalaliDate-Python
+
+## Python , کتابخانه‌ی تاریخ هجری شمسی (جلالی) برای زبان پایتون
+
+
+> منبع اصلی: https://jdf.scr.ir/jdf/
+
+
+### برای سایر زبان‌های برنامه‌نویسی:
+
+مـنـبـع | زبان | مخزن گیت‌هاب
+---|---|---
+[📗 منبع](https://jdf.scr.ir/jdf/?t=c) | C | https://github.com/SCR-IR/jalaliDate-C.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=cpp) | C++ | https://github.com/SCR-IR/jalaliDate-Cpp.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=c_sharp) | C# | https://github.com/SCR-IR/jalaliDate-C_Sharp.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=dart) | Dart | https://github.com/SCR-IR/jalaliDate-Dart.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=go) | Go | https://github.com/SCR-IR/jalaliDate-Go.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=java) | Java | https://github.com/SCR-IR/jalaliDate-Java.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=java_script) | JavaScript | https://github.com/SCR-IR/jalaliDate-JavaScript.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=kotlin) | Kotlin | https://github.com/SCR-IR/jalaliDate-Kotlin.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=objective_c) | Objective_C | https://github.com/SCR-IR/jalaliDate-Objective_C.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=pascal) | Pascal | https://github.com/SCR-IR/jalaliDate-Pascal.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=perl) | Perl | https://github.com/SCR-IR/jalaliDate-Perl.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=php) | Php | https://github.com/SCR-IR/jalaliDate-Php.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=python) | Python | https://github.com/SCR-IR/jalaliDate-Python.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=r) | R | https://github.com/SCR-IR/jalaliDate-R.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=ruby) | Ruby | https://github.com/SCR-IR/jalaliDate-Ruby.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=swift) | Swift | https://github.com/SCR-IR/jalaliDate-Swift.git
+[📗 منبع](https://jdf.scr.ir/jdf/?t=visual_basic) | VisualBasic6 | https://github.com/SCR-IR/jalaliDate-VisualBasic.git
---
+++ src/converter.py
@@ -0,0 +1,65 @@
+
+# Gregorian & Jalali ( Hijri_Shamsi , Solar ) Date Converter  Functions
+# Author: JDF.SCR.IR =>> Download Full Version :  http://jdf.scr.ir/jdf
+# License: GNU/LGPL _ Open Source & Free :: Version: 2.80 : [2020=1399]
+# ---------------------------------------------------------------------
+# 355746=361590-5844 & 361590=(30*33*365)+(30*8) & 5844=(16*365)+(16/4)
+# 355666=355746-79-1 & 355668=355746-79+1 &  1595=605+990 &  605=621-16
+# 990=30*33 & 12053=(365*33)+(32/4) & 36524=(365*100)+(100/4)-(100/100)
+# 1461=(365*4)+(4/4)   &   146097=(365*400)+(400/4)-(400/100)+(400/400)
+
+
+def gregorian_to_jalali(gy, gm, gd):
+ g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+ if (gm > 2):
+  gy2 = gy + 1
+ else:
+  gy2 = gy
+ days = 355666 + (365 * gy) + ((gy2 + 3) // 4) - ((gy2 + 99) // 100) + ((gy2 + 399) // 400) + gd + g_d_m[gm - 1]
+ jy = -1595 + (33 * (days // 12053))
+ days %= 12053
+ jy += 4 * (days // 1461)
+ days %= 1461
+ if (days > 365):
+  jy += (days - 1) // 365
+  days = (days - 1) % 365
+ if (days < 186):
+  jm = 1 + (days // 31)
+  jd = 1 + (days % 31)
+ else:
+  jm = 7 + ((days - 186) // 30)
+  jd = 1 + ((days - 186) % 30)
+ return [jy, jm, jd]
+
+
+def jalali_to_gregorian(jy, jm, jd):
+ jy += 1595
+ days = -355668 + (365 * jy) + ((jy // 33) * 8) + (((jy % 33) + 3) // 4) + jd
+ if (jm < 7):
+  days += (jm - 1) * 31
+ else:
+  days += ((jm - 7) * 30) + 186
+ gy = 400 * (days // 146097)
+ days %= 146097
+ if (days > 36524):
+  days -= 1
+  gy += 100 * (days // 36524)
+  days %= 36524
+  if (days >= 365):
+   days += 1
+ gy += 4 * (days // 1461)
+ days %= 1461
+ if (days > 365):
+  gy += ((days - 1) // 365)
+  days = (days - 1) % 365
+ gd = days + 1
+ if ((gy % 4 == 0 and gy % 100 != 0) or (gy % 400 == 0)):
+  kab = 29
+ else:
+  kab = 28
+ sal_a = [0, 31, kab, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+ gm = 0
+ while (gm < 13 and gd > sal_a[gm]):
+  gd -= sal_a[gm]
+  gm += 1
+ return [gy, gm, gd]
